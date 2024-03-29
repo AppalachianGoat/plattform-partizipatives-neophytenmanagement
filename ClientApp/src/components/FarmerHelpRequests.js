89baf972aaff8a003155c762eaf03edda3056a29
Nicholas Greensmith <123564396+VerdantForge@users.noreply.github.com>
2024-03-29 18:45:55 +0100

    WIP refactor and call backend

--- ClientApp/src/components/FarmerHelpRequests.js
+++ ClientApp/src/components/FarmerHelpRequests.js
@@ -1,58 +1,68 @@
-// This component serves as the page to display the available farmer help requests that fit the user's search criteria.
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FarmerHelpRequestsFilter } from './FarmerHelpRequestsFilter';
 import { FarmerHelpRequestsTable } from './FarmerHelpRequestsTable';
-import { FarmerHelpRequestsMap } from './FarmerHelpRequestsMap';
+import { FarmerHelpRequestsClient } from '../api/FarmerHelpServiceClients.js';
 
-export class FarmerHelpRequests extends Component {
-    constructor(props)  {
-        super(props);
-        this.state = { farmerHelpRequests: [
-             {
-                "id": 4,
-                "ownerUser": null,
-                "startDate": "2024-03-23T00:12:45.105Z",
-                "endDate": "2024-03-24T00:12:45.105Z",
-                "location": {
+export const FarmerHelpRequests = () => {
+
+    const [farmerHelpFilter, setFilter] = useState({
+        PLZ: '',
+        Umkreis: 10,
+        Anfangsdatum: Date.now(),
+        Enddatum: '',
+        Einsatzdauer: 1,
+        AnzPersonen: 1
+    });
+
+    const [farmerHelpRequests, setFarmerHelpRequests] = useState([
+        {
+            "id": 4,
+            "ownerUser": null,
+            "startDate": "2024-03-23T00:12:45.105Z",
+            "endDate": "2024-03-24T00:12:45.105Z",
+            "location": {
                 "id": 3,
                 "locationString": "1006",
                 "latitude": 46.51,
                 "longitude": 6.63
-                },
-                "workVolume": 0,
-                "numberOfHelpers": 0,
-                "invasiveSpeciesTypes": null
             },
-            {
-                "id": 5,
-                "ownerUser": null,
-                "startDate": "2024-03-23T00:12:45.105Z",
-                "endDate": "2024-03-30T00:12:45.105Z",
-                "location": {
+            "workVolume": 0,
+            "numberOfHelpers": 0,
+            "invasiveSpeciesTypes": null
+        },
+        {
+            "id": 5,
+            "ownerUser": null,
+            "startDate": "2024-03-23T00:12:45.105Z",
+            "endDate": "2024-03-30T00:12:45.105Z",
+            "location": {
                 "id": 4,
                 "locationString": "1700",
                 "latitude": 46.68,
                 "longitude": 7.08
-                },
-                "workVolume": 3,
-                "numberOfHelpers": 3,
-                "invasiveSpeciesTypes": null
-            }
-        ], loading: true };
-    }
+            },
+            "workVolume": 3,
+            "numberOfHelpers": 3,
+            "invasiveSpeciesTypes": null
+        }
+    ]);
+    
+    useEffect(() => {
+        async function getNewTableData() {
+            const currentTable = await FarmerHelpRequestsClient.getFarmerHelpRequests(farmerHelpFilter);
+            setFarmerHelpRequests(currentTable);
+        }
+        getNewTableData()
+    }, [farmerHelpFilter])
 
-    handleFilterChange = (event) => {
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Jetzt Zäme sammle</h1>
-                <p>Als Verein, Institution, etc. könnt ihr eure HIlfe anbieten oder in bestehenden Sammelaufrufen die passende Aktion auswählen</p>
-                <FarmerHelpRequestsFilter onFilterChange={this.handleFilterChange} />
-                <FarmerHelpRequestsTable farmerHelpRequests={this.state.farmerHelpRequests} />  
-                {/* <FarmerHelpRequestsMap farmerHelpRequests={this.state.farmerHelpRequests} />     */}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Jetzt Zäme sammle</h1>
+            <p>Als Verein, Institution, etc. könnt ihr eure HIlfe anbieten oder in bestehenden Sammelaufrufen die passende Aktion auswählen</p>
+            <FarmerHelpRequestsFilter filter={farmerHelpFilter} onFilterChange={setFilter} />
+            <FarmerHelpRequestsTable farmerHelpRequests={farmerHelpRequests} />  
+        </div>
+    );
 }
+
+export default FarmerHelpRequests;
--- ClientApp/src/components/FarmerHelpRequestsFilter.js
+++ ClientApp/src/components/FarmerHelpRequestsFilter.js
@@ -1,63 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import "./FarmerHelpRequestsFilter.css"
-export class FarmerHelpRequestsFilter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            farmerHelpRequestsFilter: {
-                PLZ: '',
-                Umkreis: 10,
-                Anfangsdatum: Date.now(),
-                Enddatum: '',
-                Einsatzdauer: 1,
-                AnzPersonen: 1
-            }
-        };
-    }
 
-    handleChange = (event) => {
-        this.setState({
-            farmerHelpRequestsFilter: {
-                ...this.state.farmerHelpRequestsFilter,
-                [event.target.name]: event.target.value
-            }
+export const FarmerHelpRequestsFilter = ({filter, onFilterChange}) => {
+    const handleChange = (event) => {
+        onFilterChange({
+            ...filter,
+            [event.target.name]: event.target.value
         });
     }
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.onFilterChange(this.state.farmerHelpRequestsFilter);
+        onFilterChange(filter);
     }
 
-    render() {
-        return (
-            <div id="farmer-help-request-filter">
-                <form onSubmit={this.handleSubmit}>
-                    <div id="filter-inputs">
-                        <ul>
-                            <li>
-                                <input type="text" name="PLZ" placeholder="PLZ" onChange={this.handleChange} />
-                            </li>
-                            <li>
-                                <input type="number" name="Umkreis" placeholder="Umkreis" onChange={this.handleChange} />
-                            </li>
-                            <li>
-                                <input type="date" name="Anfangsdatum" placeholder="Anfangsdatum" onChange={this.handleChange} />
-                            </li>
-                            <li>
-                                <input type="date" name="Enddatum" placeholder="Enddatum" onChange={this.handleChange} />
-                            </li>
-                            <li>
-                                <input type="number" name="Einsatzdauer" placeholder="Einsatzdauer" onChange={this.handleChange} step="0.5" />
-                            </li>     
-                            <li>
-                                <input type="number" name="AnzPersonen" placeholder="Anz. Personen" onChange={this.handleChange} />
-                            </li>
-                        </ul>
-                    </div>
-                    <button type="submit" class="neophyten-button" id="filter-button">Filter</button>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div id="farmer-help-request-filter">
+            <form onSubmit={handleSubmit}>
+                <div id="filter-inputs">
+                    <ul>
+                        <li>
+                            <input type="text" name="PLZ" placeholder="PLZ" onChange={handleChange} />
+                        </li>
+                        <li>
+                            <input type="number" name="Umkreis" placeholder="Umkreis" onChange={handleChange} />
+                        </li>
+                        <li>
+                            <input type="date" name="Anfangsdatum" placeholder="Anfangsdatum" onChange={handleChange} />
+                        </li>
+                        <li>
+                            <input type="date" name="Enddatum" placeholder="Enddatum" onChange={handleChange} />
+                        </li>
+                        <li>
+                            <input type="number" name="Einsatzdauer" placeholder="Einsatzdauer" onChange={handleChange} step="0.5" />
+                        </li>     
+                        <li>
+                            <input type="number" name="AnzPersonen" placeholder="Anz. Personen" onChange={handleChange} />
+                        </li>
+                    </ul>
+                </div>
+                <button type="submit" className="neophyten-button" id="filter-button">Filter</button>
+            </form>
+        </div>
+    );
 }
+
+export default FarmerHelpRequestsFilter;
